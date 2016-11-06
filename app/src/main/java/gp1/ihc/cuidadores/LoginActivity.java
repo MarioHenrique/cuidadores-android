262df76a9f6ad6a13a8ccc6b74782c667cc82f63
Izaac Silva <izcss21@gmail.com>
2016-11-06 12:22:14 -0200

    Conexão do Login com Web Service.

--- app/src/main/java/gp1/ihc/cuidadores/LoginActivity.java
+++ app/src/main/java/gp1/ihc/cuidadores/LoginActivity.java
@@ -4,6 +4,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import br.com.softcare.cuidadores.client.CuidadoresCliente;
+import br.com.softcare.cuidadores.exceptions.BusinessException;
+import gp1.ihc.cuidadores.ws.WebServices;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,7 +21,16 @@ public class LoginActivity extends AppCompatActivity {
 
     public void entrar(View view) {
         Intent intentBusca = new Intent(this, BuscaActivity.class);
-        startActivity(intentBusca);
+        String email = ((EditText) findViewById(R.id.login_email)).getText().toString();
+        String senha = ((EditText) findViewById(R.id.login_senha)).getText().toString();
+        try {
+            WebServices.cuidadores.login(email, senha);
+            startActivity(intentBusca);
+        } catch (BusinessException e) {
+            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+
+
     }
 
     public void cadastrar(View view) {
---
+++ app/src/main/java/gp1/ihc/cuidadores/ws/WebServices.java
@@ -0,0 +1,13 @@
+package gp1.ihc.cuidadores.ws;
+
+import br.com.softcare.cuidadores.client.CuidadoresCliente;
+
+/**
+ * Created by izcss on 06/11/2016.
+ */
+
+public class WebServices {
+
+    public static CuidadoresCliente cuidadores = new CuidadoresCliente();
+
+}
